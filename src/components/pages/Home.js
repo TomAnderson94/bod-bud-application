fa38af78a1a1474f488ddc4a5474d049dcf3964c
TomAnderson94 <137895998+TomAnderson94@users.noreply.github.com>
2023-10-22 21:26:36 +0100

    API and fetch implementation

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,41 @@
-
+import { useEffect, useState } from "react";
+import callFetch from "../api/API.js";
 
 function Home() {
+    // Initialisation ----------------------------------------
+    //const loggedInUserID = 279;
+    const endpoint = `/exerciseTypes`;
+
+    // State -------------------------------------------------
+    const [exerciseTypes, setExerciseTypes] = useState(null);
+    const [loadingMessage, setLoadingMessage] = useState('Loading records...');
+
+
+
+
+    // Context -----------------------------------------------
+    // Methods -----------------------------------------------
+    const apiCall = async (endpoint) => {
+        const result = await callFetch(endpoint, 'GET');
+        setExerciseTypes(result);
+    };
+
+    useEffect(() => { apiCall(endpoint) }, [endpoint]);
+
+
+    // View --------------------------------------------------
     return (
-        <h1>Home</h1>
+        <section>
+        <h1>Exercise Types</h1>
+        {
+          !exerciseTypes
+            ? <p>{loadingMessage}</p>
+            : exerciseTypes.length === 0
+              ? <p> No Exercise Types found</p>
+              : exerciseTypes.map((exerciseType) => 
+                <p> key={exerciseType.id}{exerciseType.exerciseTypeName}</p>)
+        }
+        </section>
     )
 }
 
