08c49ad7d89eb30f0e77a7872454847dd3c2db75
TomAnderson94 <137895998+TomAnderson94@users.noreply.github.com>
2023-10-22 21:41:12 +0100

    Fetch implementation (#3)
    * Added router, routers, route navLink, link
    
    * API and fetch implementation
    
    * fetch refactoring

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,44 @@
-
+import { useEffect, useState } from "react";
+import callFetch from "../api/API.js";
+import API from '../api/API.js';
 
 function Home() {
+    // Initialisation ----------------------------------------
+    //const loggedInUserID = 279;
+    const endpoint = `/exerciseTypes`;
+
+    // State -------------------------------------------------
+    const [exerciseTypes, setExerciseTypes] = useState(null);
+    const [loadingMessage, setLoadingMessage] = useState('Loading records...');
+
+
+
+
+    // Context -----------------------------------------------
+    // Methods -----------------------------------------------
+    const apiCall = async (endpoint) => {
+        const response = await API.get(endpoint);
+        response.isSuccess
+          ? setExerciseTypes(response.result)
+          : setLoadingMessage(response.message) 
+    };
+
+    useEffect(() => { apiCall(endpoint) }, [endpoint]);
+
+
+    // View --------------------------------------------------
     return (
-        <h1>Home</h1>
+        <section>
+        <h1>Exercise Types</h1>
+        {
+          !exerciseTypes
+            ? <p>{loadingMessage}</p>
+            : exerciseTypes.length === 0
+              ? <p> No Exercise Types found</p>
+              : exerciseTypes.map((exerciseType) => 
+                <p> key={exerciseType.id}{exerciseType.exerciseTypeName}</p>)
+        }
+        </section>
     )
 }
 
