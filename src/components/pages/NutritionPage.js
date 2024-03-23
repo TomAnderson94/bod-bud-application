7b67eee1eb386c71d83ae28da06a0107d7ce6dfd
TomAnderson94 <137895998+TomAnderson94@users.noreply.github.com>
2024-03-23 14:16:05 +0000

    Implementing custom plan link in Nutritions Page

--- src/components/pages/NutritionPage.js
+++ src/components/pages/NutritionPage.js
@@ -1,31 +1,73 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './NutritionPage.css';
+import API from '../api/API';
+import RoutineList from '../entities/RoutineList';
 
 function NutritionPage() {
 
     // Initialisation ------------------------------------
+
+    const navigate = useNavigate();
 
     // State ---------------------------------------------
     const [weight, setWeight] = useState('');
     const [height, setHeight] = useState('');
     const [age, setAge] = useState('');
     const [message, setMessage] = useState('');
+    const [routine, setRoutine] = useState(null);
+    const [selectedRoutine, setSelectedRoutine] = useState(null);
 
     // Methods -------------------------------------------
 
-    // Handlers ------------------------------------------
-    const handleCalculate = () => {
-        if (weight > 90 && age > 25 && height < 6) {
-            setMessage('It seems like you might be slightly overweight. Please refer to the Weight Loss Plan.');
-        } else if (weight > 80 && age > 21 && height < 6) {
-            setMessage('It seems like you might be slightly overweight. Please refer to the Weight Loss Plan.');
-        } else if (weight > 100 && age > 0 && height > 0) {
-            setMessage('Well damnnnnn... I mean... let me refer you to the Extreme Weight Loss Plan.');
-        } else if ((weight > 60 && weight < 90) && (age > 18) && (height > 5.5 && height < 6.6)) {
-        setMessage("Don't worry you're optimal. Please refer to yourself as King.");
-        } else {
-            setMessage('Do you really want to know?');
+    const fetchRoutineByName = async (routineName) => {
+        try {
+            console.log("by name: ", routineName);
+            const encodedRoutineName = encodeURIComponent(routineName);
+            const response = await API.get(`/routines/RoutineName/${encodedRoutineName}`);
+            if (response.isSuccess && response.result.length > 0) {
+                return response.result;
+            } else {
+                return null;
+            }
+        } catch (error) {
+            console.error('Error fetching routine by name: ', error);
+            return null;
         }
+    };
+
+    // Handlers ------------------------------------------
+    const handleCalculate = async () => {
+        try {
+            if (weight > 90 && age > 20 && height < 6.1) {
+                const routineName = 'Weight Loss Plan';
+                console.log("routine Name: ", routineName);
+                const foundRoutine = await fetchRoutineByName(routineName);
+                console.log("found routine is: ", foundRoutine);
+                setMessage('It seems like you might be slightly overweight. Please refer to the Weight Loss Plan.');
+                setRoutine(foundRoutine);
+                } else if (weight > 100 && age > 0 && height > 0) {
+                    const extremeRoutineName = 'Extreme Weight Loss Plan';
+                    const newFoundRoutine = await fetchRoutineByName(extremeRoutineName);
+                    setMessage('Well damnnnnn... I mean... let me refer you to the Extreme Weight Loss Plan.');
+                    setRoutine(newFoundRoutine);                    
+                } else if ((weight > 60 && weight < 90) && (age > 18) && (height > 5.5 && height < 6.6)) {
+                    setMessage("Don't worry you're optimal. Please refer to yourself as King.");
+                    setRoutine(null);                    
+                } else {
+                    setMessage('Do you really want to know?');
+                    setRoutine(null);                    
+                }
+        } catch (error) {
+            console.error('Error fetching routine: ', error);
+        }
+    };
+
+    const handleRoutineClick = (routine) => {
+        setSelectedRoutine(routine);
+        console.log('clicked: ', routine);
+        const routineDetailsURL = `/routines/${routine.RoutineID}`;
+        navigate(routineDetailsURL);
     };
 
     // View ----------------------------------------------
@@ -66,6 +108,15 @@ function NutritionPage() {
             </div>
             <button className='calculate-button' onClick={handleCalculate}>Calculate</button>
             {message && <p>{message}</p>}
+            {routine && (
+                <div className='recommended-plan-container'>
+                    <h2>Recommended Plan:</h2>
+                    <RoutineList
+                    routines={routine}
+                    onItemClick={handleRoutineClick}
+                    />
+                </div>
+            )}
         </div>
     );
 
